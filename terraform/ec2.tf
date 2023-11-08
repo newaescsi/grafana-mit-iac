d784a30f772d7077bcf1e7e279ea8175cb292a31
martin <martin@techstarter.de>
2023-11-08 11:17:48 +0100

    terraform creates the infrastructure

---
+++ terraform/ec2.tf
@@ -0,0 +1,66 @@
+# ec2
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "main" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  key_name                    = "playground"
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_grafana.id]
+
+  tags = {
+    Name = "Grafana-EC2"
+  }
+}
+
+# security group
+resource "aws_security_group" "allow_ssh_grafana" {
+  name        = "allow_ssh_grafana"
+  description = "allow_ssh_grafana"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Grafana"
+    from_port        = 3333
+    to_port          = 3333
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_grafana"
+  }
+}
